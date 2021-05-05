b75416e239033d9de38aa8a3609bb8165977a3a0
Sumbo Francisco <Sumbo.Francisco@gmail.com>
2021-05-05 19:45:35 +0100

    This is a sixth commit to convert the JAR to WAR for the WEB App

--- src/main/java/com/example/servingwebcontent/GreetingController.java
+++ src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -3,15 +3,22 @@ package com.example.servingwebcontent;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
 
-	@GetMapping("/greeting")
+	/*@GetMapping("/greeting")
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
 		return "greeting";
+	}*/
+	
+	@RequestMapping("/serving-web-content-0.0.1-SNAPSHOT")
+    String helloWorld() {
+    //public String index() {
+    	return "Hello World This is Sumbo Capstone Project!";
 	}
 
 }
