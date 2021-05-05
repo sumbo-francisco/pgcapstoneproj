5fce44e25949b6e7d4fb73d439658f8e93bd62d5
Sumbo Francisco <Sumbo.Francisco@gmail.com>
2021-05-05 11:51:59 +0100

    This is a commit to convert the JAR to WAR for the WEB App

--- src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
+++ src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -1,13 +1,29 @@
 package com.example.servingwebcontent;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 @SpringBootApplication
-public class ServingWebContentApplication {
+public class ServingWebContentApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ServingWebContentApplication.class);
+	}
+	
 
     public static void main(String[] args) {
         SpringApplication.run(ServingWebContentApplication.class, args);
+    }
+    
+    @RequestMapping("/")
+    String helloWorld( ) {
+    	return "Hello World!";
     }
 
 }
